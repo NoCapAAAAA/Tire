38e6cd55ac0c1c7dda477364f31567a01b382077
KaretnikovArtem(notProgrammer) <62402252+Tablecup@users.noreply.github.com>
2023-04-02 01:23:46 +0300

    подключил шаблон для панели администрирования организацией увидимся следующим летом

--- organization/models.py
+++ organization/models.py
@@ -35,3 +35,25 @@ class QuantityOfTires(models.Model):
         verbose_name = 'Количество шин'
 
 
+from django.conf import settings
+
+
+class Status(models.TextChoices):
+    CREATED = 'Создан', 'Создан'
+    CANCELLED = 'Отменён', 'Отменён'
+    WESTORE = 'На хранении', 'На хранении'
+    COMLETED = 'Завершён', 'Завершён'
+    INWORK = 'Передан в работу', 'Передан в работу'
+    DONE = 'Готов к получению', 'Готов к получению'
+
+
+class TireStore(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name="Клиент", on_delete=models.CASCADE)
+    quantity = models.ForeignKey(verbose_name='Количество', to=QuantityOfTires, on_delete=models.CASCADE)
+    size = models.ForeignKey(verbose_name='Размер шин', to=TireSize, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=0,blank=True)
+    period = models.ForeignKey(verbose_name='Период хранение', to=PeriodOfStorage, on_delete=models.CASCADE)
+    status = models.CharField('Статус', default='Ожидает подтверждения', max_length=30,
+                              choices=Status.choices, blank=True)
+    def __str__(self):
+        return f'{self.user}|{self.price}'
